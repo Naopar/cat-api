8149b8727cfe9ea1003f84884c54579c0cc8bb8c
Naopar <2201110187gx@cyber-u.ac.jp>
2025-06-12 00:11:18 +0900

    Update app.js

--- cu/app.js
+++ cu/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const path = require('path'); // pathモジュールを追加
+const path = require('path');
 const app = express();
 const port = 30074;
 
