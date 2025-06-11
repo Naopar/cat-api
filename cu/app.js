cbd9f43e79dd39254951e4bfe40c3e8d7ff5d329
Naopar <2201110187gx@cyber-u.ac.jp>
2025-06-12 00:03:56 +0900

    Update app.js

--- cu/app.js
+++ cu/app.js
@@ -3,18 +3,21 @@ const axios = require('axios');
 const app = express();
 const port = 30074;
 
-app.use(express.static('cat'));
+app.use(express.static('public'));
+
+// ルートパスのハンドラー
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/cat/cat.html'); // index.htmlを返す
+});
 
 app.get('/api/random', async (req, res) => {
     try {
         const dogResponse = await axios.get('https://dog.ceo/api/breeds/image/random');
-        const yesNoResponse = await axios.get('https://yesno.wtf/api');
-        const qrCode = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example';
+        const catResponse = await axios.get('https://api.thecatapi.com/v1/images/search');
         
         res.json({
             dogImage: dogResponse.data.message,
-            yesNo: yesNoResponse.data,
-            qrCode: qrCode
+            catImage: catResponse.data[0].url
         });
     } catch (error) {
         res.status(500).send('Error fetching data');
